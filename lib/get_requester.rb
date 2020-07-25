804cbaf7f69f74e8962283ca9c6345bf36927293
Ryna Truong <rynatruong@outlook.com>
2020-07-25 01:39:39 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -2,7 +2,13 @@ require 'net/http'
 require 'open-uri'
 require 'json'
 
+attr_accessor :url
+
 class GetRequester
+  
+  def initialize(url)
+    @url = url
+  end
   
   def get_response_body
     uri = URI.parse(URL)
